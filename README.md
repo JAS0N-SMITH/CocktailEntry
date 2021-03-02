72ff2ef0657215e55c2246ac255a5b4a93080a60
HuchKnows <47354545+HuchKnows@users.noreply.github.com>
2021-03-01 20:49:54 -0500

    UI pretty fleshed out. Working on list of ideas and things to do

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CocktailEntry
 A simple data entry interface for cocktail recipes
+
+TODO: 
+
+<h2>Ideas</h2>
+<ul>
+<li>Crop images feature</li>
+<li>Auto sizing image canvas</li>
+<li>Image renaming and name suggestion</li>
+</ul>
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
+from PIL import ImageTk, Image
 from cocktail import Cocktail
 
 window = Tk()
@@ -36,6 +38,51 @@ def add_ingredient():
     window.update()
 
 
+def del_ingredient():
+    ingredients_listvar
+
+
+def get_image():
+    # image
+    # https://www.c-sharpcorner.com/blogs/basics-for-displaying-image-in-tkinter-python
+    image_frame.canvas = Canvas(image_frame, width=300, height=400, bg="grey")
+    image_frame.canvas.grid(column=0, row=3)
+    file_path = filedialog.askopenfilename()
+    img = crop_image(img=Image.open(file_path))
+    print_image_size(img)
+    bg_image = ImageTk.PhotoImage(scale_image(img=img))
+    print(bg_image.width())
+    print(bg_image.height())
+    image_frame.canvas.bg_image = bg_image
+    image_frame.canvas.create_image(150, 200, image=image_frame.canvas.bg_image)
+
+
+def crop_image(img):
+    """https://stackoverflow.com/questions/52375035/cropping-an-image-in-tkinter"""
+    h, w = img.size
+    print_image_size(img)
+    left = w / 4
+    right = 3 * w / 4
+    upper = h / 4
+    lower = 3 * h / 4
+    return img.crop([left, upper, right, lower])
+
+
+def scale_image(img):
+    """https://www.codegrepper.com/code-examples/python/how+to+resize+image+in+python+tkinter"""
+    h, w = img.size
+    img_scale = .45
+    print_image_size(img)
+    w = round(w * img_scale)
+    h = round(h * img_scale)
+    return img.resize((h, w), Image.ANTIALIAS)
+
+
+def print_image_size(img):
+    h, w = img.size
+    print(f"Width: {w} Height: {h}")
+
+
 # UI
 window.title("Cocktail Entry")
 window.minsize(width=800, height=450)
@@ -60,14 +107,13 @@ directions_frame.columnconfigure(0, weight=10)
 directions_frame.grid_propagate(False)
 
 image_frame = ttk.Labelframe(frame, text="Image", borderwidth=5, relief='raised', width=300,
-                             height=350, padding="12 12 12 12")
+                             height=400, padding="12 12 12 12")
 image_frame.grid(column=3, row=0, padx=5, pady=5, sticky=(N, W))
 
 # labels
 """title_lable = Label(frame, text="Cocktail Entry", font=("Arial", 16, "bold"))
 title_lable.config(pady=20)
 title_lable.grid(column=0, row=0)"""
-
 
 # input
 input_name = Entry(entry_frame)
@@ -85,23 +131,23 @@ input_image.grid(column=0, row=1)
 
 # buttons
 add_drink_button = Button(entry_frame, text="Add", command=add_cocktail)
-add_drink_button.grid(column=1, row=1)
+add_drink_button.grid(column=1, row=1, sticky=(N, W, E), padx=3, pady=2)
 
 add_ingredient_button = Button(ingredient_frame, text="Add", command=add_ingredient)
-add_ingredient_button.grid(column=1, row=1)
+add_ingredient_button.grid(column=1, row=1, sticky=(N, W, E), padx=3, pady=2)
+
+del_ingredient_button = Button(ingredient_frame, text="Del", command=del_ingredient)
+del_ingredient_button.grid(column=1, row=2, sticky=(N, W, E), padx=3, pady=2)
+
+get_image_button = Button(image_frame, text="Img", command=get_image)
+get_image_button.grid(column=1, row=1, sticky=(N, W, E), padx=3, pady=2)
 
 # listbox
 ingredients_listbox = Listbox(ingredient_frame, listvariable=ingredients_listvar)
 ingredients_listbox.grid(column=0, row=2)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-window.mainloop()
+    window.mainloop()
